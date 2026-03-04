2f954e02bf15d4078a656880da4e1511c7eb0bb2
aruzhankhismetova-web <aruzhankhismetova@gmail.com>
2026-03-04 16:01:04 +0500

    add

--- Kh.Aru.Task2/Program.cs
+++ Kh.Aru.Task2/Program.cs
@@ -6,55 +6,55 @@
     static void Main(string[] args)
     {
         //// Exercize 1
-        //Console.WriteLine("Exercise 1:");
-        //Console.Write("Enter first number: ");
-        //double n1 = Convert.ToDouble(Console.ReadLine());
-        //Console.Write("Enter second number: ");
-        //double n2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Exercise 1:");
+        Console.Write("Enter first number: ");
+        double n1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        double n2 = Convert.ToDouble(Console.ReadLine());
 
-        //if (n1 == n2)
-        //{
-        //    Console.WriteLine("The numbers are equal.");
-        //}
-        //else if (n1 > n2)
-        //{
-        //    Console.WriteLine("The first number is greater than the second.");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("The first number is less than the second.");
-        //}
-        //Console.WriteLine("--------------------");
+        if (n1 == n2)
+        {
+            Console.WriteLine("The numbers are equal.");
+        }
+        else if (n1 > n2)
+        {
+            Console.WriteLine("The first number is greater than the second.");
+        }
+        else
+        {
+            Console.WriteLine("The first number is less than the second.");
+        }
+        Console.WriteLine("--------------------");
 
         //Exercize 2
-        //Console.WriteLine("Exercise 2:");
-        //Console.Write("Enter a number: ");
-        //double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Exercise 2:");
+        Console.Write("Enter a number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
 
-        //if (num2 > 5 && num2 < 10)
-        //{
-        //    Console.WriteLine("The number is greater than 5 and less than 10");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Unknown number");
-        //}
-        //Console.WriteLine("--------------------");
+        if (num2 > 5 && num2 < 10)
+        {
+            Console.WriteLine("The number is greater than 5 and less than 10");
+        }
+        else
+        {
+            Console.WriteLine("Unknown number");
+        }
+        Console.WriteLine("--------------------");
 
         //// Exercize 3
-        //Console.WriteLine("Exercise 3:");
-        //Console.Write("Enter a number: ");
-        //double num3 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Exercise 3:");
+        Console.Write("Enter a number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
 
-        //if (num3 == 5 || num3 == 10)
-        //{
-        //    Console.WriteLine("The number is either 5 or 10");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Unknown number");
-        //}
-        //Console.WriteLine("--------------------");
+        if (num3 == 5 || num3 == 10)
+        {
+            Console.WriteLine("The number is either 5 or 10");
+        }
+        else
+        {
+            Console.WriteLine("Unknown number");
+        }
+        Console.WriteLine("--------------------");
 
         // Exercize 4
         Console.WriteLine("Exercise 4:");
